7eb857d3dcf0c1ddcb9f1b76e39dd494157a829a
KristinaBrak <31766262+KristinaBrak@users.noreply.github.com>
2018-04-10 19:09:14 +0300

    added spinning cat

--- js/game.js
+++ js/game.js
@@ -1,16 +1,19 @@
-var game = new Phaser.Game(600, 800, Phaser.AUTO, 'game');
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game');
 
+var cat;
 
 var GameState = {
   preload: function() {
-
+game.load.image('pusheenCat', 'assets/pusheenCat.jpg');
   },
 
   create: function() {
-
+    cat = game.add.sprite(game.world.centerX, game.world.centerY, 'pusheenCat');
+    cat.anchor.setTo(0.5, 0.5); //puts image to center
+    //s.rotation = -0.14;
   },
   update: function() {
-
+    cat.angle += 1; //kampas po 1 laipsni
   }
 }
 
